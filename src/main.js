a0e35c3b679b387dfd485653db1a67bf7796a612
Frodo1980 <34411791+Frodo1980@users.noreply.github.com>
2020-11-01 12:08:26 +0100

    Fix: Prettier und Eslint arbeitet jetzt fehlerfrei zusammen

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-import firebase from "firebase/app";
+import firebase from 'firebase/app'
 
 // Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth'
+import 'firebase/firestore'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   created: function() {
-    alert("Created-Lifecycle-Hook wird ausgeführt");
+    alert('Created-Lifecycle-Hook wird ausgeführt')
     const firebaseConfig = {
-      apiKey: "AIzaSyBGm-VFXIZpcNJbZVxslCN9tIWmJ7iM73w",
-      authDomain: "vue-testapp-5f009.firebaseapp.com",
-      databaseURL: "https://vue-testapp-5f009.firebaseio.com",
-      projectId: "vue-testapp-5f009",
-      storageBucket: "vue-testapp-5f009.appspot.com",
-      messagingSenderId: "583321436893",
-      appId: "1:583321436893:web:0e3fe456fc1a2e52b01134",
-      measurementId: "G-KKBZJTWP0H",
-    };
+      apiKey: 'AIzaSyBGm-VFXIZpcNJbZVxslCN9tIWmJ7iM73w',
+      authDomain: 'vue-testapp-5f009.firebaseapp.com',
+      databaseURL: 'https://vue-testapp-5f009.firebaseio.com',
+      projectId: 'vue-testapp-5f009',
+      storageBucket: 'vue-testapp-5f009.appspot.com',
+      messagingSenderId: '583321436893',
+      appId: '1:583321436893:web:0e3fe456fc1a2e52b01134',
+      measurementId: 'G-KKBZJTWP0H'
+    }
 
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig)
   },
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
