b6150a559285dab860897e25bb720e8379446136
Frodo1980 <34411791+Frodo1980@users.noreply.github.com>
2020-11-01 13:02:47 +0100

    Prerender-Plugin eingefügt

--- src/main.js
+++ src/main.js
@@ -33,5 +33,6 @@ new Vue({
   },
 
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered'))
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ['/', '/home', '/about'],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 }
